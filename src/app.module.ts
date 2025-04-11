006ba353d07a6909854f2b8fc690cecc57336d3f
dzhivanyan <dzhivanyan>
2025-04-11 17:17:53 +0300

    update app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { ConfigModule } from '@nestjs/config'
+import * as process from 'node:process'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const port = process.env.PORT ?? 3000
+  app.set('query parser', 'extended')
+  app.setGlobalPrefix('api')
+  await app.listen(port)
+  console.log(`Server running on port ${port}`)
 }
 
 void bootstrap()
